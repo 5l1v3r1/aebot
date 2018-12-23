5b6383cca0e7d4812754831812a94f59f24e1882
Duc Phan <ducphanduy.agentp@gmail.com>
2018-12-23 10:06:34 +0700

    Add configs. Add power control to modules

--- aemodules/troll_duy.js
+++ aemodules/troll_duy.js
@@ -2,6 +2,10 @@ let DuyID = "100000379068861";
 let messageList = ["Duy câm mòm"];
 
 function response(api, message) {
+    if (global.configs.power_status == 0) {
+        return;
+    }
+    
     if (message.senderID == DuyID) {
         api.sendMessage(messageList[0], message.threadID);
     }
--- aemodules/troll_hieu.js
+++ aemodules/troll_hieu.js
@@ -2,6 +2,10 @@ let HieuID = "100002606806680";
 let messageList = ["Hiếu câm mòm"];
 
 function response(api, message) {
+    if (global.configs.power_status == 0) {
+        return;
+    }
+    
     if (message.senderID == HieuID) {
         api.sendMessage(messageList[0], message.threadID);
     }
